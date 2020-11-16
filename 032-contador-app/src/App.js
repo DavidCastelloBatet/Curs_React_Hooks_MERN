03ef89aaa4a1d843de460370665974585ace7d2e
David Castello <davidcastellobatet@gmail.com>
2020-11-16 08:58:10 +0100

    aprenentatge useState hook

--- 032-contador-app/src/App.js
+++ 032-contador-app/src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
+
+const App = ({value}) => {
+
+  //Hooks
+  const [counter, setCounter] = useState(value);
+
+  //Handles
+  const handleAdd = () => {
+    setCounter( counter + 1);
+    console.log(`Valor del counter pre-click: ${counter}, valor del value inicial del useState: ${value}`);
+  }
+  const handleResset = () => {    
+    setCounter( value );
+    console.log(`Valor del counter pre-click: ${counter}, valor del value inicial del useState: ${value}`);
+
+  }
+  const handleSubstract = () => {
+    setCounter( counter - 1);
+    console.log(`Valor del counter pre-click: ${counter}, valor del value inicial del useState: ${value}`);
+  }
 
   return (
-    <p>Hola Contador App</p>
+    <div className='mainContainer'>
+      <div className='textos'>
+        <h1>CounterApp</h1>
+        <h2>{counter}</h2>
+      </div>
+      <div className='buttons'>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleResset}>Reset</button>
+        <button onClick={handleSubstract}>-1</button>
+        {console.log(`Valor del counter post-click: ${counter}, valor del value inicial del useState: ${value}`)}
+      </div>
+    </div>
   )
+}
+
+App.propTypes = {
+  value: PropTypes.number.isRequired,
 }
 
 export default App;
